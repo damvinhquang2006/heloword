6639134cf76320c40561025cb8423184f707da42
damvinhquang2006 <damvinhquang0406@gmail.com>
2025-08-24 23:06:05 +0700

    first commit

---
+++ heloword.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std ;
+
+int main(){
+    cout<< "mmbvcl";
+    cout<< "mmbvcl";
+    cout<< "mmbvcl";
+    cout<< "mmbvcl";
+    cout<< "mmbvcl";
+    cout<< "cha m gray";
+   return 0;
+}

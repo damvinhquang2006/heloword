b72023a50ec6db8121b635262676716e7f90b08a
damvinhquang2006 <damvinhquang0406@gmail.com>
2025-08-25 00:25:28 +0700

    chinh sua 1 ti

--- heloword.cpp
+++ heloword.cpp
@@ -10,15 +10,45 @@ int linear_search(int a[], int n, int x){
     return -1;
 }
 
+int binary_search(int a[], int n, int x) {
+    int left = 0, right = n - 1;
+    while (left <= right) {
+        int mid = (left + right) / 2;
+
+        if (a[mid] == x)
+            return mid; 
+        else if (a[mid] < x)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return -1; 
+}
+
 
 int main(){
-   int arr[] = {1, 2, 3, 4, 5};
-   int n = sizeof(arr) / sizeof(arr[0]);
-   int x = 3;
-   int result = linear_search(arr, n, x);
-   if(result != -1)
-       cout << "So can tim la: " << result << endl;
-   else
-       cout << "-Ko tim thay so can tim-" << endl;
-   return 0;
+    int a[] = {1,2,3,4,5,6,7,8,9,10};
+    int n = sizeof(a)/ sizeof(a[0]);
+
+    int x;
+    cout <<"Hay nhap vao so ban can tim:"; 
+    cin >> x;
+
+    int kq1 = linear_search(a,n,x);
+    int kq2 = binary_search(a,n,x);
+
+    if(kq1 != -1 ){
+        cout << "Ket qua tim kiem tuyen tinh: " << kq1 << endl;
+    } else {
+        cout << "Khong tim thay ket qua tuyen tinh." << endl;
+    }
+
+    if(kq2 != -1 ){
+        cout << "Ket qua tim kiem nhi phan: " << kq2 << endl;
+    } else {
+        cout << "Khong tim thay ket qua nhi phan." << endl;
+    }
+
+    return 0;
 }
+
